129ec22d83cc444b7d88092d1436b6f17d68d8f7
Ronald Chipana <ronald.chipana.w@gmail.com>
2024-03-22 16:18:04 -0400

    Rutas - RequestParam Parametros

--- src/main/java/com/codesign/tutorial/Rutas.java
+++ src/main/java/com/codesign/tutorial/Rutas.java
@@ -2,6 +2,7 @@ package com.codesign.tutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class Rutas {
     @GetMapping("/libro/{id}/nombre/{nombre}")
     String leerLibro(@PathVariable int id, @PathVariable String nombre){
         return "Leyendo el libro con id:" + id + ", de nombre: " + nombre;
+    }
+
+    @GetMapping("/libro2/{id}")
+    String leerLibro2(@PathVariable int id, @RequestParam String params, @RequestParam String editorial){
+        return "Leyendo el libro2 con id:" + id + ", params: " + params + ", de editorial: " + editorial;
     }
 }
