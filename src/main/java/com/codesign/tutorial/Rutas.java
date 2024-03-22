cc4a6780c9101cbfa44ae2ce6de1ee94529d39c6
Ronald Chipana <ronald.chipana.w@gmail.com>
2024-03-22 13:20:45 -0400

    Rutas - Parametros de ruta

--- src/main/java/com/codesign/tutorial/Rutas.java
+++ src/main/java/com/codesign/tutorial/Rutas.java
@@ -1,6 +1,7 @@
 package com.codesign.tutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class Rutas {
     @GetMapping("/hola")
     String miPrimeraRuta(){
         return "Hola mundo desde Spring Controller :)";
+    }
+
+    @GetMapping("/libro/{id}/nombre/{nombre}")
+    String leerLibro(@PathVariable int id, @PathVariable String nombre){
+        return "Leyendo el libro con id:" + id + ", de nombre: " + nombre;
     }
 }
